54f7c0b1254cfc1c8074ec97f1763700fa7b34ec
shout <shout@host.docker.internal>
2024-03-04 13:18:26 +0900

    data.sqlのテストデータ修正

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO employee(code,name,role,password,delete_flag,created_at,updated_at)
-     VALUES ("1","煌木　太郎","ADMIN","$2a$10$US2gK9Bvmepvs95OWk29peAUe7rA9rvghQEYP4ZXcTxg/NJ8m8dJ6",0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+     VALUES ("1","煌木　太郎","ADMIN","$2a$08$clh9XaYYznpX9WDqySgiCuUu4znpSeu2oJi5l2Q00UJs42Llrbd7S",0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO employee(code,name,role,password,delete_flag,created_at,updated_at)
-     VALUES ("2","田中　太郎","GENERAL","$2a$10$HPIjRCymeRZKEIq.71TDduiEotOlb8Ai6KQUHCs4lGNYlLhcKv4Wi",0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+     VALUES ("2","田中　太郎","GENERAL","$2a$10$F1k.2HZtkRpoSDymdZCTnuI7eVdoKP.Yb8gtiWmVTKejp53Htlm56",0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
